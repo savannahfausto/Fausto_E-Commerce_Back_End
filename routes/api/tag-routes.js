ab132e5598114be850453c54993be3a21485d428
Savannah Fausto <savannahfausto@Savannahs-MBP.hsd1.ca.comcast.net>
2022-10-06 18:30:09 -0700

    fixes routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -50,9 +50,9 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update({ where: { id: req.params.id }},{
+    const tagData = await Tag.update({
       tag_name: req.body.tag_name,
-    });
+    }, { where: { id: req.params.id }});
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
