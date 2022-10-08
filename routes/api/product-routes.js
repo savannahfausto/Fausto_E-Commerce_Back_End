e8b2d64d06f77cf3dc91a5e9b26532242ad9c03d
Savannah Fausto <savannahfausto@Savannahs-MBP.attlocal.net>
2022-10-08 11:36:10 -0700

    fixes put routes four product and tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productName = await Category.findByPk(req.params.id, {
+    const productName = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, {model: Tag, through: ProductTag}],
     });
 
